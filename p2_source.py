4450ed30ca7121856fe59eb1ed4f1edc1458ef36
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-07-01 23:01:06 -0600

    stretch question 1 start

--- p2_source.py
+++ p2_source.py
@@ -146,3 +146,34 @@ q3_plot = (
         y="Park Changes per 100 Years",
     )
 )
+
+s1_query = """
+SELECT
+  f.playerID,
+  f.yearID,
+  f.POS,
+  f.G,
+  s.salary
+FROM
+  fielding AS f
+  INNER JOIN salaries AS s
+ON
+  s.yearID = f.yearID
+  AND s.playerID = f.playerID
+WHERE
+  f.ID = (
+    SELECT
+      f2.ID
+    FROM
+      fielding AS f2
+    WHERE
+      f2.playerID = f.playerID
+      AND f2.yearID = f.yearID
+    ORDER BY
+      f2.G DESC
+    LIMIT
+      1
+  )
+"""
+
+s1_data = pd.read_sql_query(s1_query, db)
--- test.sql
+++ test.sql
@@ -1,80 +1,26 @@
 SELECT
-  (people.nameFirst || ' ' || people.nameLast) AS "Player Name",
-  schools.name_full AS School,
-  salaries.yearID as Year,
-  salaries.salary as Salary,
-  teams.name AS Team
+  f.playerID,
+  f.yearID,
+  f.POS,
+  f.G,
+  s.salary
 FROM
-  people
-  JOIN collegePlaying ON people.playerID = collegePlaying.playerID
-  JOIN schools ON collegePlaying.schoolID = schools.schoolID
-  JOIN salaries ON people.playerID = salaries.playerID
-  JOIN teams ON salaries.teamID = teams.teamID
+  fielding AS f
+  INNER JOIN salaries AS s
 WHERE
-  schools.schoolID = "idbyuid"
-GROUP BY
-  people.playerID,
-  salaries.salary,
-  salaries.yearID,
-  salaries.teamID;
-
-SELECT
-  batting.playerID,
-  (people.nameFirst || ' ' || people.nameLast) AS "Player Name",
-  teams.name,
-  batting.yearID,
-  batting.AB AS "At Bats",
-  batting.H AS "Hits",
-  CAST(batting.H AS FLOAT) / CAST(batting.AB AS FLOAT) AS "Batting Average"
-FROM
-  batting
-  INNER JOIN people ON batting.playerID = people.playerID
-  INNER JOIN teams ON batting.team_ID = teams.ID
+  s.yearID = f.yearID
+  AND s.playerID = f.playerID
 WHERE
-  batting.AB >= 1
-ORDER BY
-  "Batting Average" DESC,
-  batting.playerID ASC
-LIMIT
-  5;
-
-SELECT
-  batting.playerID,
-  (people.nameFirst || ' ' || people.nameLast) AS "Player Name",
-  teams.name,
-  SUM(batting.AB) AS "Total At Bats",
-  SUM(batting.H) AS "Total Hits",
-  CAST(SUM(batting.H) AS FLOAT) / CAST(SUM(batting.AB) AS FLOAT) AS "Career Batting Average"
-FROM
-  batting
-  INNER JOIN people ON batting.playerID = people.playerID
-  INNER JOIN teams ON batting.team_ID = teams.ID
-GROUP BY
-  batting.playerID,
-  people.nameFirst,
-  people.nameLast,
-  teams.name
-HAVING
-  "Total At Bats" >= 100
-ORDER BY
-  "Career Batting Average" DESC,
-  batting.playerID ASC
-LIMIT
-  5;
-
-SELECT
-  teams.teamID,
-  teams.name,
-  COUNT(DISTINCT teams.park) - 1 AS "lifetime_park_changes",
-  COUNT(*) AS "total_years_active",
-  CAST(COUNT(DISTINCT teams.park) AS FLOAT) / COUNT(*) * 100 AS "park_changes_100_year_average"
-FROM
-  teams
-WHERE
-  teams.park IS NOT NULL
-GROUP BY
-  teams.teamID
-HAVING
-  "lifetime_park_changes" >= 1
-ORDER BY
-  "park_changes_100_year_average" ASC;
+  f.ID = (
+    SELECT
+      f2.ID
+    FROM
+      fielding AS f2
+    WHERE
+      f2.playerID = f.playerID
+      AND f2.yearID = f.yearID
+    ORDER BY
+      f2.G DESC
+    LIMIT
+      1
+  )
